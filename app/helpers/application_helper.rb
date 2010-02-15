25241db87e5074d2da81a10be18a217d5009c392
viswaraj <viswaraj@brightal.com>
2010-02-15 20:51:53 +0530

    fixed titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def title(title)
-    if @title.blank?
-      @title=""
-    end
-    content_tag 'title',"#{@title}/FriendsApp"
+  def title (title)
+    
+    content_for (:title) {title}
   end
 end
